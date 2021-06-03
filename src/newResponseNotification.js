52ae1600af9379a7c8e1e56c02e322801022e4bd
mayim-chayim <mayimchayim0@gmail.com>
2021-06-03 11:52:03 -0400

    change popup below last post

--- src/newResponseNotification.js
+++ src/newResponseNotification.js
@@ -15,16 +15,24 @@ let checkNewResponse = setInterval(function () {
 				let newCount = (data.match(/class="post has-profile/g) || []).length;
 				let isNewPage = (data.match(/<li class="next">/g) || []).length > 0;
 				if (currentCount < newCount) {
-					document.body.innerHTML +=
-						'<div class="navbar" id="reload" style="background-color: #0f75a7;position:fixed;top: 80%;width: 20%;left: 75%;"><h1 style="margin: auto;text-align: center;">נייע תגובה איז יעצט אריינגעקומען.<br><a class="button" onclick="window.location.reload();">איבערלאדן</a></h1></div>';
+					lastPost.insertAdjacentHTML('afterend', 
+                        `<h3 style="margin: auto;text-align: center;">
+                            נייע תגובה איז יעצט אריינגעקומען
+                            <br>
+                            <a class="button" onclick="window.location.reload();">איבערלאדן</a>
+                        </h3>`)
 
 					let markUnreadUrl = data.match(/\/forum\/app\.php\/markpostunread\/\d+\/\d+/g)[currentCount];
 					fetch(markUnreadUrl);
                     
 					clearInterval(checkNewResponse);
 				}else if(isNewPage){
-					document.body.innerHTML +=
-							'<div class="navbar" id="reload" style="background-color: #0f75a7;position:fixed;top: 80%;width: 20%;left: 75%;"><h1 style="margin: auto;text-align: center;"> פרישע בלאט איז יעצט צוגעקומען.<br><a class="button" ' + (data.match(/href=".*" rel="next"/g) || [])[0] + ">גיי צו פרישע בלאט</a></h1></div>";
+					lastPost.insertAdjacentHTML('afterend', 
+                    `<h3 style="margin: auto;text-align: center;">
+                        פרישע בלאט איז יעצט צוגעקומען
+                        <br>
+                        <a class="button" ${(data.match(/href=".*" rel="next"/g) || [])[0]}>גיי צו פרישע בלאט</a>
+                    </h3>`)
 	                clearInterval(checkNewResponse);
                 }
 			},
