833960e42c7c497fe3075ee9e57b9ad36b275349
Sunegi <85598386+Sunegi@users.noreply.github.com>
2021-06-23 00:44:18 +0100

    Update contentScript.js

--- extension/js/contentScript.js
+++ extension/js/contentScript.js
@@ -1,26 +1,26 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('keyboardShortcuts.js');
+s.src = chrome.extension.getURL('js/keyboardShortcuts.js');
 (document.head || document.documentElement).appendChild(s);
 s.onload = function () {
     s.parentNode.removeChild(s);
 };
 
 var u = document.createElement('script');
-u.src = chrome.extension.getURL('addQuoteLastOnlyButton.js');
+u.src = chrome.extension.getURL('js/addQuoteLastOnlyButton.js');
 (document.head || document.documentElement).appendChild(u);
 u.onload = function () {
     u.parentNode.removeChild(u);
 }
 
 var t = document.createElement('script');
-t.src = chrome.extension.getURL('removeNestedQuotes.js');
+t.src = chrome.extension.getURL('js/removeNestedQuotes.js');
 (document.head || document.documentElement).appendChild(t);
 t.onload = function () {
     t.parentNode.removeChild(t);
 }
 
 var n = document.createElement('script');
-n.src = chrome.extension.getURL('newResponseNotification.js');
+n.src = chrome.extension.getURL('js/newResponseNotification.js');
 (document.head || document.documentElement).appendChild(n);
 n.onload = function () {
     n.parentNode.removeChild(n);
