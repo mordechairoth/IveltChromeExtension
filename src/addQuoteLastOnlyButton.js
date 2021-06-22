06ad00c3223f87c302d394bde2833081948ca33b
Sunegi <85598386+Sunegi@users.noreply.github.com>
2021-06-22 22:54:14 +0100

    Update addQuoteLastOnlyButton.js
    Added `user-select: none;`

--- src/addQuoteLastOnlyButton.js
+++ src/addQuoteLastOnlyButton.js
@@ -5,7 +5,7 @@ function addBtn(){
     let a = document.createElement('a');
     let href = btn.querySelector('a.button.icon-button.quote-icon').getAttribute('href');
     a.setAttribute('href', href + '&last=true');
-    a.setAttribute('style', "height:18px; width:12px; margin:0; padding: 0 5px; color:black; text-align:center;");
+    a.setAttribute('style', "height:18px;width:12px;margin:0;padding:0 5px;color:black;text-align:center;user-select:none;");
     a.setAttribute('class', 'button');
     a.setAttribute('title', 'ציטיר בלויז די לעצטע תגובה');
     li.appendChild(a);
