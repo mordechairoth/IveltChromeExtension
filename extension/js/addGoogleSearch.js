251f6aec9c754d4b41257ade83732d32638cd760
Knaper Yaden <87161728+KnaperYaden@users.noreply.github.com>
2021-09-03 14:15:01 -0400

    Added Google Site Search

--- extension/js/addGoogleSearch.js
+++ extension/js/addGoogleSearch.js
@@ -1,4 +1,3 @@
-
 function addGoogleSearch(){
     if (window.location.href.includes("viewtopic.php")) {
         let topicTitle = document.querySelectorAll('.topic-title > a')[0].text;
@@ -18,3 +17,19 @@ function addGoogleSearch(){
 }
 
 addGoogleSearch();
+
+function addGoogleSiteSearch(){
+    let searchUrl = `https://www.google.com/search?q=site%3Aivelt.com+`
+        document.getElementById('search').insertAdjacentHTML('beforebegin', 
+            '<form action="./search.php" method="get" id="g_site_search_keywords"><fieldset><input name="keywords2" id="keywords2" type="search" class="inputbox search tiny" style="border-width:1px;border-radius:4px" size="200" value="" placeholder="גוגל זוך אין אייוועלט"></fieldset></form><br>'
+        ) 
+
+        document.getElementById('g_site_search_keywords').addEventListener('keydown' ,function (e) {
+            if (e.key === 'Enter') {
+                window.open(searchUrl + document.getElementById('keywords2').value, '_blank').focus();
+                e.preventDefault();
+            }
+        });
+}
+
+addGoogleSiteSearch();
