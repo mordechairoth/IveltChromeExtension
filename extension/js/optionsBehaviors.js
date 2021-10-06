7bd1afd96f39e9e9434b5c9e8b8b7fd8d68ba86e
nefshech <92034146+nefshech@users.noreply.github.com>
2021-10-06 18:12:56 -0400

    Hide the avatar image
    Co-authored-by: mayim-chayim <85243488+mayim-chayim@users.noreply.github.com>

--- extension/js/optionsBehaviors.js
+++ extension/js/optionsBehaviors.js
@@ -2,11 +2,9 @@
 
 	if(chrome.storage){
 		chrome.storage.sync.get(['hideUserName'], function(items){
-
-			console.log(items.hideUserName)
-
 			if(items.hideUserName){
 				document.querySelector('.header-avatar .username').innerText = 'Ha?';
+				document.querySelector('.header-avatar .avatar').style.display = "none";
 			}
 		});
 	}
