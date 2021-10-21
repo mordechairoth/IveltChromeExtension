41b1250ebd19dce96527fe52c7b1e346bcb4e45d
nefshech <nefshech@gmail.com>
2021-10-20 22:10:12 -0400

    remove sample notification

--- extension/js/settings.js
+++ extension/js/settings.js
@@ -8,12 +8,7 @@ function startListeners(){
 
 	// get browser notification checkbox
 	document.getElementById('getBrowserNotifications').addEventListener('change', (e) => {
-		const getBrowserNotifications = !!e.currentTarget.checked;
-		
-		commitNewSetting({getBrowserNotifications: getBrowserNotifications});
-
-		if(getBrowserNotifications)
-			chrome.runtime.sendMessage({title: 'אייוועלט נאטיפיקאציע', message: 'דאס איז א סעמפל אייוועלט נאטיפיקאציע', type: 'browserNotification'});
+		commitNewSetting({getBrowserNotifications: !!e.currentTarget.checked});
 	});
 }
 
@@ -23,27 +18,27 @@ function commitNewSetting(nameValue){
 	chrome.storage.sync.set(nameValue, function() {
 		// Let user know options were saved.
 		const fadeTarget = document.getElementById('notify');
-	    fadeTarget.classList.add('fade');
-	    setTimeout(function() {
-	        fadeTarget.classList.remove('fade');
-	    }, 3650);
+		fadeTarget.classList.add('fade');
+		setTimeout(function() {
+			fadeTarget.classList.remove('fade');
+		}, 3650);
 	});
 }
 
 // Restores settings options using the preferences
 // stored in chrome.storage.
 function initSettings() {
-  
-  // Get preferences or set defaults
-  chrome.storage.sync.get({
-	hideUserName: false,
-	getBrowserNotifications: false
-  }, function(items) {
-	document.getElementById('hideUserName').checked = items.hideUserName;
-	document.getElementById('getBrowserNotifications').checked = items.getBrowserNotifications;
 
-	startListeners();
-  });
+	// Get preferences or set defaults
+	chrome.storage.sync.get({
+		hideUserName: false,
+		getBrowserNotifications: false
+	}, function(items) {
+		document.getElementById('hideUserName').checked = items.hideUserName;
+		document.getElementById('getBrowserNotifications').checked = items.getBrowserNotifications;
+
+		startListeners();
+	});
 }
 
 document.addEventListener('DOMContentLoaded', initSettings);
