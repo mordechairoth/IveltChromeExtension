0c3605d31f8dcd2ee35d98174472157311cf4f5a
mayim-chayim <85243488+mayim-chayim@users.noreply.github.com>
2021-12-30 14:02:54 -0500

    add www to google search (#57)

--- extension/js/addGoogleSearch.js
+++ extension/js/addGoogleSearch.js
@@ -1,7 +1,7 @@
 function addGoogleSearch(){
     if (window.location.href.includes("viewtopic.php")) {
         let topicTitle = document.querySelectorAll('.topic-title > a')[0].text;
-        let searchUrl = `https://www.google.com/search?q=intitle%3A"${topicTitle}"+site%3Aivelt.com+`
+        let searchUrl = `https://www.google.com/search?q=intitle%3A"${topicTitle}"+site%3Awww.ivelt.com+`
         document.getElementById('search_keywords').insertAdjacentHTML('beforebegin', 
             '<input class="inputbox search tiny" style="border-width:1px;border-radius:4px" type="search" id="g_search_keywords" size="20" placeholder="גוגל זוך אין דעם אשכול…">'
         ) 
@@ -19,7 +19,7 @@ function addGoogleSearch(){
 addGoogleSearch();
 
 function addGoogleSiteSearch(){
-    let searchUrl = `https://www.google.com/search?q=site%3Aivelt.com+`
+    let searchUrl = `https://www.google.com/search?q=site%3Awww.ivelt.com+`
         document.getElementById('search').insertAdjacentHTML('beforebegin', 
             '<input id="g_site_search_keywords" type="search" class="inputbox search" style="border-radius:4px;margin-bottom: 5px;" placeholder="גוגל זוך אין אייוועלט">'
         ) 
