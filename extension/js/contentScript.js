9f563b18b2ffb7d148342c701bfabb30bb9647f4
KF MDM Solutions <contact@kfmdmsolutions.com>
2022-02-03 15:32:40 -0500

    Update contentScript.js

--- extension/js/contentScript.js
+++ extension/js/contentScript.js
@@ -1,15 +1,17 @@
 let scripts = [
     "js/keyboardShortcuts.js",
     "js/removeNestedQuotes.js",
-    "js/addQuoteLastOnlyButton.js",
     "js/newResponseNotification.js",
     "js/addGoogleSearch.js",
-    "js/settingsHandler.js"
+    "js/settingsHandler.js",
+	"js/add_button.js"
+
 ];
+
 
 scripts.forEach(s => {
     let e = document.createElement('script');
-    e.src = chrome.extension.getURL(s);
+    e.src = chrome.runtime.getURL(s);
     (document.head || document.documentElement).appendChild(e);
     e.onload = function () {
         e.parentNode.removeChild(e);
